cea1fb17ab523b9b4b6999946653222240ad69db
Alex Kumpula <alex.kumpula01@gmail.com>
2026-05-12 07:12:43 -0600

    test

--- modules/niri.nix
+++ modules/niri.nix
@@ -4,4 +4,5 @@
 
   # For wrapper niri
   services.displayManager.sessionPackages = [ inputs.xalaynix-wrappers.packages.${pkgs.system}.niri-noctalia ];
+  systemd.packages = [ inputs.xalaynix-wrappers.packages.${pkgs.system}.niri-noctalia ];
 }
